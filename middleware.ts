b5e219ad3f082f158bc47f8e80efab74668c79d8
Vaibhav Singh <singh12521vaibhav@gmail.com>
2026-05-27 23:36:54 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
+  const res = NextResponse.next(); 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
